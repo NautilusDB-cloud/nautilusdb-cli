e4e0ed38ccce1e0497d8299e2df969b23268b80b
Ryan Kang <rkang@Ryans-MacBook-Air.local>
2023-10-05 16:07:10 -0700

    fix cli usage text

--- src/nautiluscli/nautiluscli.py
+++ src/nautiluscli/nautiluscli.py
@@ -53,20 +53,20 @@ def run(action, collection, file: UrlOrFile, query):
 
     \b
     1. Create a new Collection `myCollection` in the shared demo account
-    >>> poetry run python nautiluscli.py create-collection myCollection
+    >>> nautiluscli create-collection myCollection
 
     \b
     2. Index a PDF into `myCollection`. In this example, we will index the original research paper on Transformers.
-    >>> poetry run python nautiluscli.py upsert-vectors myCollection --file=https://arxiv.org/pdf/1706.03762.pdf
+    >>> nautiluscli upsert-vectors myCollection --file=https://arxiv.org/pdf/1706.03762.pdf
 
     \b
     3. Alternatively, upload a PDF for indexing. Note that demo account and all Collections are publicly accessible
        , so please do not upload anything sensitive!
-    >>> poetry run python nautiluscli.py upsert-vectors myCollection --file=README.md
+    >>> nautiluscli upsert-vectors myCollection --file=README.md
 
     \b
     4. Query a Collection to get answers!
-    >>> poetry run python nautiluscli.py ask myCollection --query="what is a transformer?"
+    >>> nautiluscli ask myCollection --query="what is a transformer?"
     """
     t0 = time.monotonic()
     api_endpoint = DEMO_API_ENDPOINT
